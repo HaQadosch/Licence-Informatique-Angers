9a33cc894026ac502894f53a34194f0a03a2efe6
BrinonBat <baptiste.brinon.etu@univ-lemans.fr>
2018-10-17 09:55:47 +0200

    Update TP3_BDD.sql

--- TP3_BDD.sql
+++ TP3_BDD.sql
@@ -35,52 +35,27 @@ WHERE qualif='ASSISTANT'
 OR qualif='CHEF DE CLINIQUE');
 
 --4bis
-SELECT cdprs,nom,prnm,qualif FROM PERSONNELS
-WHERE qualif='ASSISTANT' 
-OR qualif='CHEF DE CLINIQUE'
-OR cdprs IN 
-(SELECT cdspr FROM PERSONNELS
-WHERE qualif='ASSISTANT' 
-OR qualif='CHEF DE CLINIQUE');
+SELECT e.cdprs,e.nom,e.prnm,e.qualif,s.cdprs,s.nom,s.prnm,s.qualif  FROM PERSONNELS e JOIN PERSONNELS s on e.cdspr=s.cdprs
+WHERE e.qualif='ASSISTANT' 
+OR e.qualif='CHEF DE CLINIQUE';
 
 --5
 SELECT PERSONNELS.nom,prnm,sx,datnais,SERVICES.nom 
 FROM PERSONNELS JOIN SERVICES ON PERSONNELS.cdsrv=SERVICES.cdsrv
 WHERE qualif='CHEF DE SERVICE';
 
---6
-SELECT S.nom,
-CASE 
- WHEN P.nom IS NULL THEN 'NULL'
- ELSE P.nom 
-END
-,CASE
- WHEN prnm IS NULL THEN 'NULL' 
- ELSE prnm 
-END
-,CASE
- WHEN sx IS NULL THEN 'NULL' 
- ELSE sx 
-END
-FROM PERSONNELS P JOIN SERVICES S ON P.cdsrv=S.cdsrv
-WHERE qualif='CHEF DE SERVICE';
+--6)
+select s.nom as nomserv,p.nom ,p.prnm,p.sx
+from (SELECT *from PERSONNELS where(qualif='CHEF DE SERVICE')) p right outer join SERVICES s
+on p.cdsrv=s.cdsrv;
 
---7
-SELECT S.nom,
-CASE 
- WHEN P.nom IS NULL THEN '****'
- ELSE P.nom 
-END
-,CASE
- WHEN prnm IS NULL THEN '****' 
- ELSE prnm 
-END
-,CASE
- WHEN sx IS NULL THEN '****' 
- ELSE sx 
-END
-FROM PERSONNELS P JOIN SERVICES S ON P.cdsrv=S.cdsrv
-WHERE qualif='CHEF DE SERVICE';
+--7)
+select s.nom as nomserv,
+coalesce(p.nom,'****') as nom,
+coalesce(p.prnm,'****') as prnm,
+coalesce(p.sx,'****') as sx
+from (SELECT *from PERSONNELS where(qualif='CHEF DE SERVICE')) p right outer join SERVICES s
+on p.cdsrv=s.cdsrv;
 
 --8
 
