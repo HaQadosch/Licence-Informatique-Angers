d95447278ee76ab95c6f83e847b2c0cd5449fc3a
BrinonBat <baptiste.brinon.etu@univ-lemans.fr>
2018-10-24 09:47:24 +0200

    Ajout exercice 1

--- TP3_BDD.sql
+++ TP3_BDD.sql
@@ -1,4 +1,51 @@
 ------------ EXERCICE 1-------------
+
+--1
+SELECT DISTINCT nomf, c.nomp ,prix
+FROM FOURNISSEUR f JOIN PRODUIT p ON f.numf = p.numf JOIN COMMANDE c ON p.nomp = c.nomp JOIN CLIENT cl ON cl.numc = cl.numc
+WHERE cl.nomc = 'Jean';
+
+--2
+SELECT DISTINCT nomf, c.nomp ,prix
+FROM  FOURNISSEUR f JOIN PRODUIT p ON f.numf = p.numf JOIN COMMANDE c ON p.nomp = c.nomp JOIN CLIENT cl ON cl.numc = cl.numc
+WHERE cl.nomc = 'Jean'
+ORDER BY nomp DESC, nomf ASC;
+
+--3
+SELECT nomp 
+FROM  PRODUIT
+WHERE prix = ( SELECT MIN(prix) FROM PRODUIT);
+
+--4
+SELECT nomp, AVG(prix) moy
+FROM  PRODUIT
+GROUP BY nomp;
+
+--5
+SELECT nomp, AVG(prix) moy
+FROM PRODUIT
+GROUP BY nomp
+HAVING AVG(prix) > 1000;
+
+--6
+SELECT nomp, AVG(prix) moy
+FROM  PRODUIT
+GROUP BY nomp
+HAVING AVG(prix) > (SELECT AVG(prix)
+FROM PRODUIT;
+                    
+--7
+SELECT nomp, AVG(prix) moy
+FROM  PRODUIT
+GROUP BY nomp
+HAVING COUNT (prix) > 2;
+                    
+--8
+SELECT p.nomp, AVG(prix) moy
+FROM  FOURNISSEUR f JOIN PRODUIT p ON f.numf = p.numf JOIN COMMANDE c ON p.nomp = c.nomp JOIN CLIENT cl ON cl.numc = cl.numc
+GROUP BY p.nomp , cl.villec , f.villef
+HAVING cl.villec LIKE '%Paris' AND f.villef LIKE '%ANGERS';
+
 ------------ EXERCICE 2-------------
 
 --1
@@ -28,7 +75,7 @@ SELECT nom,prnm FROM PATIENTS;
 
 --4
 SELECT e.cdprs,e.nom,e.prnm,e.qualif,s.cdprs,s.nom,s.prnm,s.qualif 
-FROM PERSONNELS e JOIN PERSONNELS s on e.cdspr=s.cdprs
+FROM PERSONNELS e JOIN PERSONNELS s ON e.cdspr=s.cdprs
 WHERE e.qualif='ASSISTANT' 
 OR e.qualif='CHEF DE CLINIQUE';
 
@@ -49,13 +96,11 @@ coalesce(p.prnm,'****') as prnm,
 coalesce(p.sx,'****') as sx
 FROM 
 (SELECT * FROM PERSONNELS WHERE (qualif='CHEF DE SERVICE')) p RIGHT OUTER JOIN SERVICES s
-on p.cdsrv=s.cdsrv;
+ON p.cdsrv=s.cdsrv;
 
 --8
-select s.cdsrv,s.nom
-from SERVICES s
-where not exists(select *
-from PERSONNELS P
-where p.cdsrv=s.cdsrv);
+SELECT s.cdsrv,s.nom FROM SERVICES s
+WHERE NOT exists
+(SELECT *FROM  PERSONNELS P WHERE p.cdsrv=s.cdsrv);
 
 ------------ EXERCICE 3-------------
