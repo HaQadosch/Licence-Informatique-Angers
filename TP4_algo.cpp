0c9bc2733466e4280109396b7a298451335e54b1
BrinonBat <baptiste.brinon.etu@univ-lemans.fr>
2018-10-26 14:33:40 +0200

    ALED

--- TP4_algo.cpp
+++ TP4_algo.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <array>
+#include <math.h> 
 
 const int TMAX=200;
 using tab=std::array<char,TMAX>;
@@ -9,6 +10,8 @@ struct pile{
 	tab pile; 
 	int sommet;
 };
+
+
 
 void initpile(pile &p){
 	p.sommet=-1;
@@ -27,14 +30,70 @@ char depiler(pile &p){
 		c=p.pile[p.sommet];
 		p.sommet--;
 		return c;	
-	}
+	} else return(' ');
 }
 
-int pilevide(pile &p){
+bool pilevide(pile &p){
 	return(p.sommet==-1);
 }
 
+void initTP(pile &p){
+	empiler(p,'2');
+	empiler(p,'5');
+	empiler(p,'+');
+	empiler(p,'4');
+	empiler(p,'5');
+	empiler(p,'3');
+	empiler(p,'+');
+	empiler(p,'2');
+	empiler(p,'^');
+	empiler(p,'x');
+	empiler(p,'R');
+	empiler(p,'*');
+}
 
+float add(float a, float b){
+	return(a+b);
+}
+
+float mult(float a, float b){
+	return (a*b);
+}
+
+float racine(float x){
+	return(sqrt(x));
+}
+
+float puiss(float a, float n){
+	for (int i=0; i<n;i++){
+		a=a*a;	
+	}
+	return a;
+}
+
+float calcul(float res,pile &p){
+	char a;
+	if(!pilevide(p)){
+		std::cout<<res<<std::endl;
+		a=depiler(p);
+		if(a=='+'){
+			res=add(res,calcul(res,p));
+		}else if(a=='*'){
+			res=mult(res,calcul(res,p));
+		}else if(a=='R'){
+			res=racine(calcul(res,p));
+		}else if(a=='^'){
+			res=puiss(res,calcul(res,p));
+		}else res=(a-'0');
+	}
+	return (res);
+}
 
 int main(){
+	pile p;
+	initpile(p);
+	initTP(p);
+	float res=0.0;
+	res=calcul(res,p);
+	std::cout<<res<<std::endl;
 }
