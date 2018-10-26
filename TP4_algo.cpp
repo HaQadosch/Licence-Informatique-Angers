4857946eec1f93b3e63573844f941de86881d25d
BrinonBat <baptiste.brinon.etu@univ-lemans.fr>
2018-10-26 13:04:38 +0200

    pile fonctionnelle

--- TP4_algo.cpp
+++ TP4_algo.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <array>
 
+const int TMAX=200;
+char pile [TMAX]; 
+int sommet;
+
+void initpile(){
+	sommet=-1;
+}
+
+void empiler(char c){
+	if(sommet<TMAX-1){
+		sommet++;
+		pile[sommet]=c;	
+	}
+}
+
+void depiler(char *c){
+	if(sommet>=0){
+		*c=pile[sommet];
+		sommet--;	
+	}
+}
+
+int pilevide(){
+	return(sommet==-1);
+}
+
+
+int main(){
+}
