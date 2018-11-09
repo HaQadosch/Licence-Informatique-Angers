ef794cb3e890925351db34233c17eaf50a2f752c
BrinonBat <baptiste.brinon.etu@univ-lemans.fr>
2018-11-09 13:45:33 +0100

    Update TP5_algo.cpp

--- TP5_algo.cpp
+++ TP5_algo.cpp
@@ -18,3 +18,38 @@ Void ajout_arete(matrice &M, int l, int c){
   M[l][c]=TRUE;
   M[c][l]=TRUE;
 }
+//QC
+unsing couleur=std::array<int,MAX_SIZE>;
+couleur coloration(int n){
+  couleur c[n];
+  for (int i=0;i<n;i++){
+    c[i]=0;
+  }
+  return c;
+}
+    
+//QD
+bool correct (Matrice M,couleur C, int n){
+  for (int i=0;i<n;i++){
+    for(int j=i;j<n;j++){
+      if(M[i][j]==TRUE && M[j][i]==TRUE){
+        if(C[i]==C[j]) return FALSE;
+      }
+    }
+  }
+  return TRUE;
+}
+    
+int main(){
+  matrice M;
+  init_matrice(M,9);
+  ajout_arete(M,1,2);
+  ajout_arete(M,2,5);
+  ajout_arete(M,5,6);
+  ajout_arete(M,6,3);
+  ajout_arete(M,5,4);
+  ajout_arete(M,4,7);
+  ajout_arete(M,5,8);
+  ajout_arete(M,8,9);
+  return 1;+
+}
