c9e3ce6db0fc861c7a99735631ce45369c80bf91
BrinonBat <baptiste.brinon.etu@univ-lemans.fr>
2018-11-16 14:08:00 +0100

    Update TP6_Algo.cpp

--- TP6_Algo.cpp
+++ TP6_Algo.cpp
@@ -13,18 +13,51 @@ struct roulette{
 
 void initialise_roulette(roulette &rou){
 	maillon * mai1=new maillon;
-	(*mai1).value=0;
+	(*mai1).value=2;
 	(*mai1).suivant=NULL;
 	rou.taille=1;
 	rou.debut=mai1;
 }
 
-void ajout_numero(
+void ajout_numero(roulette &rou, int num){
+	maillon * mai=new maillon;
+	(*mai).value=num;
+	(*mai).suivant=rou.debut;
+	for(int i=1;i<rou.taille;i++){
+		(*mai).suivant=(*mai).suivant->suivant;	
+	}
 
+	/*while((*mai).suivant->suivant!=NULL){
+		(*mai).suivant=(*mai).suivant->suivant;
+		std::cout<<"valeur dans boucle :"<<(*mai).suivant->value;
+	}
+	*/
+	(*mai).suivant->suivant=mai;
+	(*mai).suivant=NULL;
+	
+	rou.taille++;
+
+}
+
+
+// creation d'une fonction d'affichage, pour tester l'exo
+void affiche_roulette( roulette rou){
+	maillon * cur=new maillon;
+	(*cur).value=0;
+	(*cur).suivant=rou.debut;
+	for(int i=0;i<rou.taille;i++){
+		std::cout<<(*cur).suivant->value;
+		(*cur).suivant=(*cur).suivant->suivant;
+	}
+
+}
 int main(){
 	
 	roulette rou;
 	initialise_roulette(rou);
-	std::cout<<rou.debut->value<<" "<<rou.taille;
+	ajout_numero(rou,3);
+	ajout_numero(rou,4);
+	ajout_numero(rou,5);
+	affiche_roulette(rou);
 	return 0;
 }
