2d6e4ae415ae2177208edf8afb8bcab0b555d1b0
BrinonBat <baptiste.brinon.etu@univ-lemans.fr>
2018-11-16 12:58:11 +0100

    Update TP6_Algo.cpp

--- TP6_Algo.cpp
+++ TP6_Algo.cpp
@@ -1,2 +1,22 @@
 #include <iostream>
 #include <cstdlib>
+
+struct maillon{
+   int value;
+   maillon * suivant;
+};
+
+struct roulette{
+  int taille;
+  maillon * debut; 
+};
+
+void initialise_roulette(roulette &rou){
+
+
+
+}
+
+int main(){
+	return 0;
+}
