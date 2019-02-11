9289d907f36f6ebe895f63b7d6288ad52cb05518
BrinonBat <baptiste@brinon.eu>
2019-02-11 14:48:25 +0100

    Update TP3.php

--- S4/PHP/TP3.php
+++ S4/PHP/TP3.php
@@ -1,7 +1,8 @@
 //EXO1
 <?php
-$fic=fopen("./logs.txt","a+");
-if(!&fic) die("unable to create file");
-$date=date("d/M H:m:s",time());
-fwrite(&fic,&date."\n");
+	$fic=fopen("./logs.txt","a+");
+	if(!$fic) die("unable to create file");
+	$date=date("d/M H:m:s",time());
+	fwrite($fic,$date."\n");
+	fclose($fic);
 ?>
