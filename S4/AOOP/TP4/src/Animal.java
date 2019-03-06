e39af8a0ea07681706532c8482de6fefc737cb3c
BrinonBat <baptiste@brinon.eu>
2019-03-06 17:07:42 +0100

    Add files via upload

---
+++ S4/AOOP/TP4/src/Animal.java
@@ -0,0 +1,22 @@
+
+public class Animal {
+	
+	int poids;
+	int age;
+	
+	public Animal(int poids, int age) {
+		this.poids = poids;
+		this.age = age;
+	}
+	
+	public void dormir(){
+		System.out.println("Je dors \n");
+	}
+	
+	public void  manger(){
+		System.out.println("Je mange \n");
+	}
+}
+
+
+
---
+++ S4/AOOP/TP4/src/Poisson.java
@@ -0,0 +1,10 @@
+public class Poisson extends Animal{
+	
+	public Poisson(int poids, int age) {
+		super(poids, age);
+	}
+
+	public void nager(){
+		System.out.println("Je nage \n");
+	}
+}
