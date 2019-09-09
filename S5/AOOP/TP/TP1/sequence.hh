4fb40f126b1c82a7227bda14a9a7068cbb70ac29
BrinonBat <baptiste@brinon.eu>
2019-09-09 16:01:05 +0200

    Create sequence.hh

---
+++ S5/AOOP/TP/TP1/sequence.hh
@@ -0,0 +1,24 @@
+#pragma once
+#include <array>
+
+enum class couleur{
+    rouge,
+    bleu,
+    jaune,
+    vert
+};
+
+using indicesequence = unsigned int;
+
+struct sequence{
+    std::array<couleur, 100> contenu;
+    indicesequence taille;
+};
+
+void sc_initialiservide(sequence &s);
+void sc_ajouter(sequence &s,couleur coul);
+void sc_copier( sequence &vers, sequence de);
+couleur sc_acces(sequence s, indicesequence ind);
+void sc_afficher(couleur coul);
+void sc_afficher(sequence s);
+void sc_vider(sequence &s);
