ffbe3cac24f0802317590ec31e58fd89d936d51f
BrinonBat <baptiste@brinon.eu>
2019-09-11 09:01:21 +0200

    Update sequence.hh

--- S5/AOOP/TP/TP1/sequence.hh
+++ S5/AOOP/TP/TP1/sequence.hh
@@ -22,3 +22,5 @@ couleur sc_acces(sequence s, indicesequence ind);
 void sc_afficher(couleur coul);
 void sc_afficher(sequence s);
 void sc_vider(sequence &s);
+bool sc_comparer(sequence s1, sequence s2);
+
