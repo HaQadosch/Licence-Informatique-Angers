5ab51f9b8e399f68dc39a06c0548d617ae707d17
BrinonBat <baptiste@brinon.eu>
2019-11-25 21:13:45 +0100

    ajout recapitulatif CM2

--- S5/AOOP/Cours/récapitulatif/pointeurs.cc
+++ S5/AOOP/Cours/récapitulatif/pointeurs.cc
@@ -8,8 +8,12 @@
 	int maximum(std::array<int, 1000> & t); /* modifier directement le referecement, évite de faire une copie de 1000 int
 	cependant, le paramètre sera modifié. Si on veut éviter celà, on ajoute un const	*/
 	int maximum(std::array<int, 1000> const & t);
-	
+
 //allocation & destruction simple new->delete
 	int * i=new int;delete i;
 	int * i=new int(valInitiale);delete i;
 	int * t=new int[taille];delete []i;
+
+// accés aux variables
+	(*ptr).val;
+	ptr->val; // sont identiques
