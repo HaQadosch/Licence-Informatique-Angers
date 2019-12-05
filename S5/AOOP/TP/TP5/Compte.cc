081efde6b31302e455d0ec84529580fc1e4e6c63
BrinonBat <baptiste@brinon.eu>
2019-12-05 11:57:38 +0100

    ajout Comptes TP5

--- S5/AOOP/TP/TP5/Compte.cc
+++ S5/AOOP/TP/TP5/Compte.cc
@@ -1,1 +1,22 @@
 #include "Compte.hh"
+
+unsigned int Compte::Compteur(0);
+
+Compte::Compte(std::shared<proprietaire> prop, float montant){
+
+}
+
+void Compte::verser(float m){
+	montant+=m;
+}
+void cCompte::retirer(float m){
+	montant -=m;
+}
+
+void Compte::virerVers(Compte & c,float f){
+	montant -= f;
+	c.montant += f;
+	if(prop != c.prop){
+		montant-=1;
+	}
+}
--- S5/AOOP/TP/TP5/Compte.hh
+++ S5/AOOP/TP/TP5/Compte.hh
@@ -1,6 +1,15 @@
+#pragma once
+#include "proprietaire.hh"
+
 class Compte{
 	public:
-
+			Compte(std::shared_ptr<proprietaire> prop, float montant);
+			void verser (float m);
+			void retirer(float m);
+			void virerVers(Compte & c,float f);
 	private:
-
+			unsigned int numero;
+			std::shared_ptr<proprietaire> prop;
+			float montant;
+			static unsigned int compteur;
 };
