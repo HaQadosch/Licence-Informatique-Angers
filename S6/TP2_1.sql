2db86f8d7873c0ac0fb6580a3e7411165a154502
BrinonBat <baptiste@brinon.eu>
2020-01-28 10:37:00 +0100

    petite maj

--- S6/TP2_1.sql
+++ S6/TP2_1.sql
@@ -1,5 +1,5 @@
 /* création et remplissage des tables produit */
-DROP TABLE produit ;
+DROP TABLE IF EXISTS produit ;
 
 CREATE TABLE produit (
 	NumProd INT NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE produit (
 	Prix FLOAT
 );
 
-DROP TABLE produit2;
+DROP TABLE IF EXISTS produit2;
 	
 CREATE TABLE produit2 (NumProd INT NOT NULL,
 	PRIMARY KEY(NumProd),
@@ -19,7 +19,7 @@ CREATE TABLE produit2 (NumProd INT NOT NULL,
 insert into produit values (1,'une bonne moyenne',100),(2,'alibaba',NULL);
 
 /* fonction créant la table produit2 à partir de produit */
-CREATE FUNCTION construP2() returns void AS $$
+CREATE OR REPLACE FUNCTION construP2() returns void AS $$
 	DECLARE
 		cursdate cursor FOR SELECT * FROM produit;
 	BEGIN
