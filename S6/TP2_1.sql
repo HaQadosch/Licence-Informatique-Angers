71eca0aab11ca45351e02aab78576b97767f912f
BrinonBat <baptiste@brinon.eu>
2020-01-28 10:27:52 +0100

    maj pour delete produit2

--- S6/TP2_1.sql
+++ S6/TP2_1.sql
@@ -1,4 +1,4 @@
-/* création et remplissage de la table produit */
+/* création et remplissage des tables produit */
 DROP TABLE produit ;
 
 CREATE TABLE produit (
@@ -8,6 +8,14 @@ CREATE TABLE produit (
 	Prix FLOAT
 );
 
+DROP TABLE produit2;
+	
+CREATE TABLE produit2 (NumProd INT NOT NULL,
+	PRIMARY KEY(NumProd),
+	Designation VARCHAR(50),
+	Prix INT NOT NULL
+);
+
 insert into produit values (1,'une bonne moyenne',100),(2,'alibaba',NULL);
 
 /* fonction créant la table produit2 à partir de produit */
@@ -15,20 +23,16 @@ CREATE FUNCTION construP2() returns void AS $$
 	DECLARE
 		cursdate cursor FOR SELECT * FROM produit;
 	BEGIN
-		CREATE TABLE produit2 (NumProd INT NOT NULL,
-			PRIMARY KEY(NumProd),
-			Designation VARCHAR(50),
-			Prix INT NOT NULL
-		);
 		IF EXISTS(SELECT * FROM produit) THEN 
-			FOR val IN cursdate LOOP
-				IF val.prix IS NULL THEN val.prix=0 ENDIF;
-				INSERT INTO produit2 VALUES(
-					val.NumProd,
-					UPPER(val.Designation),
-					ROUND(val.Prix*(1/6.55957))
-					);
-			ENDLOOP;
+				FOR val IN cursdate LOOP
+					IF val.prix IS NULL THEN val.prix=0 ENDIF;
+					INSERT INTO produit2 VALUES(
+						val.NumProd,
+						UPPER(val.Designation),
+						ROUND(val.Prix*(1/6.55957))
+						);
+				ENDLOOP;
+
 		 ELSE 
 			INSERT INTO produit2 VALUES (0,'Pas de produit',NULL);
 		 ENDIF;
