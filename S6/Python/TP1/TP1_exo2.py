74d9a6eeafadae2a90d70fc38fc7f362b5516ae8
BrinonBat <baptiste@brinon.eu>
2020-02-03 09:15:23 +0100

    fin des array exo 2 TP1

--- S6/Python/TP1/TP1_exo2.py
+++ S6/Python/TP1/TP1_exo2.py
@@ -29,4 +29,49 @@ numExo+=1 #exercice 6#
 liste=[i for i in range(1,10)]
 arStr=np.array(str(liste))
 
-print("\n"+str(numExo)+": "+str(arStr)+" est de type"+str(type(arStr)))
+print("\n"+str(numExo)+": "+str(arStr))
+
+numExo+=1 #exercice 7#
+tab1=np.arange(1,10.1,0.5)
+
+print("\n"+str(numExo)+": "+str(tab1))
+
+numExo+=1 #exercice 8#
+tab2=np.linspace(1,10,num=20)
+
+print("\n"+str(numExo)+": "+str(tab2))
+
+numExo+=1 #exercice 9#
+tab3=np.arange(10,30)
+
+print("\n"+str(numExo)+": "+str(tab3))
+
+numExo+=1 #exercice 10#
+somme=tab3.sum()
+
+print("\n"+str(numExo)+": "+str(somme))
+
+numExo+=1 #exercice 11#
+sommecum=np.cumsum(tab3)
+
+print("\n"+str(numExo)+": "+str(sommecum))
+
+numExo+=1 #exercice 12#
+moy=tab3.mean()
+
+print("\n"+str(numExo)+": "+str(moy))
+
+numExo+=1 #exercice 13#
+prod=tab2*tab3
+
+print("\n"+str(numExo)+": "+str(prod))
+
+numExo+=1 #exercice 14#
+tab3sup20=(tab3[tab3>20])
+
+print("\n"+str(numExo)+": "+str(tab3sup20))
+
+numExo+=1 #exercice 15#
+selec=tab3[[1,5,10,15]]
+
+print("\n"+str(numExo)+": "+str(selec))
