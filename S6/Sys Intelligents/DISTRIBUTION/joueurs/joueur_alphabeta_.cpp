be695664a2c072cd5c4a54365b2fef708d650e0a
BrinonBat <baptiste@brinon.eu>
2020-02-10 14:02:47 +0100

    correction alphabeta

--- S6/Sys Intelligents/DISTRIBUTION/joueurs/joueur_alphabeta_.cpp
+++ S6/Sys Intelligents/DISTRIBUTION/joueurs/joueur_alphabeta_.cpp
@@ -15,7 +15,7 @@ char Joueur_AlphaBeta_::nom_abbrege() const
 int Joueur_AlphaBeta_::eval_E(int etatTemp,int &bestE,int &bestA,Jeu jeu){
 	//condition de défaite
 	if(jeu.etat()==0){
-		std::cout<<"OH!"<<std::endl;
+//		std::cout<<"OH!"<<std::endl;
 		return -1;
 	}
 	int v=-1000;
@@ -36,21 +36,21 @@ int Joueur_AlphaBeta_::eval_E(int etatTemp,int &bestE,int &bestA,Jeu jeu){
 int Joueur_AlphaBeta_::eval_A(int etatTemp,int &bestE,int &bestA,Jeu jeu){
 	//condition de victoire
 	if(jeu.etat()==0){
-		std::cout<<"OH!"<<std::endl;
+//		std::cout<<"OH!"<<std::endl;
 		return 1;
 	}
 //	std::cout<<"check a1"<<std::endl;
 	int v=+1000;
 	for(int etatSuiv=etatTemp, i=0;jeu.coup_licite(i);i++){ // A RETRAVAILLER s est l'etat du prochain coup
-		//std::cout<<"check a2"<<std::endl;
+//		std::cout<<"check a2"<<std::endl;
 		etatSuiv=jeu[i];
 		jeu.joue(i);
-		std::cout<<"etat: "<<jeu.etat()<<std::endl;
-		//std::cout<<"check a2.3"<<std::endl;
+//		std::cout<<"etat: "<<jeu.etat()<<std::endl;
+//		std::cout<<"check a2.3"<<std::endl;
 		v=min(v,eval_E(etatSuiv,bestE,bestA,jeu));
-		std::cout<<"check v: "<<v<<std::endl;
-		if(v>bestA){return v;}
-		//std::cout<<"check a48"<<std::endl;
+//		std::cout<<"check v: "<<v<<std::endl;
+		if(v<=bestA){return v;}
+//		std::cout<<"check a48"<<std::endl;
 		bestA=min(v,bestE);
 	}
 	return v;
@@ -58,8 +58,12 @@ int Joueur_AlphaBeta_::eval_A(int etatTemp,int &bestE,int &bestA,Jeu jeu){
 
 void Joueur_AlphaBeta_::recherche_coup(Jeu jeu, int &coup)
 {
-	int etatTemp = jeu.etat();
-	int inf=1000;
-	int neginf=-1000;
-	coup=max(0,eval_E(etatTemp,neginf,inf,jeu));
+	for(int i=0,temp=0;jeu.coup_licite(i) && temp!=1 ;i++){
+		int inf=1000;
+		int neginf=-1000;
+		temp=max(0,eval_E(jeu[i],neginf,inf,jeu));
+		if(temp==1) coup=i;
+	}
+//	coup=jeu.nb_coups() == 1 ?  0 : rand() % (jeu.nb_coups()-1);
+	std::cout<<coup<<std::endl;
 }
