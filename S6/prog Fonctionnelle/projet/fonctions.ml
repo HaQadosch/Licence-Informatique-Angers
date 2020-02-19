d95a1af02a38af5051312b98653f0b8c76e89223
BrinonBat <baptiste@brinon.eu>
2020-02-19 12:05:31 +0100

    avancée sur le parcours en profondeur et ajout de fonctions utiles

--- S6/prog Fonctionnelle/projet/fonctions.ml
+++ S6/prog Fonctionnelle/projet/fonctions.ml
@@ -8,6 +8,11 @@ let rec liste_sommets graphe = match graphe with
 	(*cas du dernier sommet*)
 	|([]) -> []
 
+(* retourne le premier sommet du graphe*)
+let rec racine graphe = match graphe with
+	((num,succs)::_) -> (num,succs)
+	|[]->failwith "graphe vide"
+
 (*retourne la liste des successeurs du sommet dans le graphe *)
 let rec liste_succ sommet ((num,succs)::suite) =
 	if (sommet=0) then failwith "sommet invalide" else
@@ -19,20 +24,32 @@ let rec liste_succ sommet ((num,succs)::suite) =
 
 (*indique si num est dans la liste ou non
 est utilisé dans le parcours pour savoir si un sommet n'a pas encore été visité*)
-let nEstPasDans num liste = match liste with
-	(x::r)-> if(x=num) then false else nEstPasDans num r
-	|([]) -> true;;
+let estDans num liste = match liste with
+	(x::r)-> if(x=num) then true else nEstPasDans num r
+	|([]) -> false;;
+(*********** estDans = List.mem ???? **************)
 
+(*retourne une liste des elements de liste1 qui ne sont pas dans liste 2*)
+let rec neSontPasDans liste1 liste2
 
 (* parcours en profondeur *)
 let parcours_profondeur graphe =
 	(* parcours des successeurs *)
 	(* s'il n'y a plus de successeurs à traiter
 	ajout de l'element à la liste des sommets visités  *)
-	let rec parcours_interne g dejaVisite=
- 		match g with
-			((num,succs)::suite) -> if(nEstPasDans num dejaVisite) then
+	let rec parcours_interne sommet dejaVisite pasEncoreVisite=
+		if (pasEncoreVisite=[]) then [] (* cas ou l'on a fini de parcourir le graphe*)
+ 		else
+		match sommet with
+			(num,succs) ->
+				if(List.mem num pasEncoreVisite) (* si le sommet n'est pas encore visité*)
+				(*alors on ajoute ses successeurs qui ne sont pas dans " deja visite " dans " pas encore visité "*)
+				(*on ajoute le sommet actuel dans la liste de ceux déjà visités*)
+				(*on visite le prochain element de pas encore visité *)
+				then num::parcours_interne ? ? (pasEncoreVisite::(neSontPasDans succs dejaVisite))
+					
 
-	in parcours_interne graphe []
+
+	in parcours_interne (racine graphe) [] [1]
 
 (* *)
