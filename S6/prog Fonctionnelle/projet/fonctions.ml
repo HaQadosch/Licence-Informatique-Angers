d108b1ffd7ebcaf5953e6d7894c28d556822e178
BrinonBat <baptiste@brinon.eu>
2020-02-25 11:31:09 +0100

    l'algo semble OK, mais il reste des syntax error à corriger

--- S6/prog Fonctionnelle/projet/fonctions.ml
+++ S6/prog Fonctionnelle/projet/fonctions.ml
@@ -13,24 +13,43 @@ let rec racine graphe = match graphe with
 	((num,succs)::_) -> (num,succs)
 	|[]->failwith "graphe vide";;
 
-(*retourne la liste des successeurs du sommet dans le graphe *)
+(* retourne le numero du sommet *)
+let numSommet sommet = match sommet with
+	(num,succs)->num;;
+
+(*retourne la liste des successeurs du sommet dans le graphe ) VER 1
 let rec liste_succ sommet ((num,succs)::suite) =
 	if (sommet=0) then failwith "sommet invalide" else
 	if (sommet=1) (* cas ou l'on est sur le bon sommet*)
 		then succs
 	else  if(((num,succs)::suite)=[]) (*cas ou le sommet saisi n'est pas dans le graphe*)
 		then failwith "sommet non présent dans le graphe"
-	else liste_succ (sommet-1) suite;; (*cas ou l'on cherche le sommet *)
+	else liste_succ (sommet-1) suite;; (*cas ou l'on cherche le sommet *) *)
+
+
+
+(*retourne la liste des successeurs de sommet dans graphe*)
+let rec successeurs graphe sommet = match graphe with
+	((x, s)::r) -> if x = sommet then s else (successeurs r sommet)
+	| [] -> failwith "Ce sommet n'appartient pas au graphe";;
+(*successeurs graphe1 3;;*)
+
 
 (*indique si num est dans la liste ou non
-est utilisé dans le parcours pour savoir si un sommet n'a pas encore été visité*)
+est utilisé dans le parcours pour savoir si un sommet n'a pas encore été visité)
 let estDans num liste = match liste with
 	(x::r)-> if(x=num) then true else nEstPasDans num r
 	|([]) -> false;;
-(*********** estDans = List.mem ???? **************)
+(*********** estDans = List.mem ???? **************) *)
 
-(*retourne une liste des elements de liste1 qui ne sont pas dans liste 2*)
-let rec neSontPasDans liste1 liste2
+(*retire les elements de l2 à l1*)
+let rec retirer l1 l2 = match l1 with
+	(x::r) -> if List.mem x l2 	then retirer r l2
+									else x :: retirer r l2
+	| [] -> [];;
+
+(*retourne une liste des elements de liste1 qui ne sont pas dans liste 2)
+let rec neSontPasDans liste1 liste2 *)
 
 (* parcours en profondeur *)
 let parcours_profondeur graphe =
@@ -38,18 +57,17 @@ let parcours_profondeur graphe =
 	(* s'il n'y a plus de successeurs à traiter
 	ajout de l'element à la liste des sommets visités  *)
 	let rec parcours_interne sommet dejaVisite pasEncoreVisite=
-		if (pasEncoreVisite=[]) then [] (* cas ou l'on a fini de parcourir le graphe*)
+		if (pasEncoreVisite=[]) then dejaVisite (* cas où l'on a fini de parcourir le graphe*)
  		else
 		match sommet with
 			(num,succs) ->
 				if(List.mem num pasEncoreVisite) (* si le sommet n'est pas encore visité*)
-				(*alors on ajoute ses successeurs qui ne sont pas dans " deja visite " dans " pas encore visité "*)
-				(*on ajoute le sommet actuel dans la liste de ceux déjà visités*)
-				(*on visite le prochain element de pas encore visité *)
-				then num::parcours_interne ? ? (pasEncoreVisite::(neSontPasDans succs dejaVisite))
+				(* 3 - alors on ajoute ses successeurs qui ne sont pas dans " déjà visité " dans " pas encore visité "*)
+				(* 3bis - on retire le sommet actuel de ceux pasEncoreVisités*)
+				(* 2 - on ajoute le sommet actuel dans la liste de ceux déjà visités*)
+				(* 1 - on visite le prochain element de pas encore visité *)
+				then (*num::*)parcours_interne List.hd(retirer (succs@pasEncoreVisite dejaVisite)) (dejaVisite::num) (retirer (succs@pasEncoreVisite) (dejaVisite::num))
+			|_ -> failwith "sommet invalide"
 
 
-
-	in parcours_interne (racine graphe) [] [1];;
-
-(* *)
+	in parcours_interne (racine graphe) [] [numSommet (racine graphe)];;
