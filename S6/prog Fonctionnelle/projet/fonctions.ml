df338ae6222a02a0c6864747cc65dbc71024adef
Xavier Brinon <xavier.brinon@ecs.co.uk>
2020-02-25 13:40:45 +0000

    ml: c'est super degeu mais ca compile 🤷🏻‍♂️

--- S6/prog Fonctionnelle/projet/fonctions.ml
+++ S6/prog Fonctionnelle/projet/fonctions.ml
@@ -1,4 +1,5 @@
 (* graphe de l'énoncé qui sera utilisé pour l'exemple *)
+(* val graphe1 : (int * int list) list = [(1, [6; 7; 8]); (2, [1; 4]); (3, [2]); (4, [3; 5]); (5, [1]); (6, [5; 7]); (7, []); (8, [6; 7])] *)
 let graphe1 = [
 	(1, [6;7;8]); 
 	(2, [1;4]);
@@ -11,6 +12,7 @@ let graphe1 = [
 ];;
 
 (*retourne la liste des sommets*)
+(* val liste_sommets : ('a * 'b) list -> 'a list = <fun> *)
 let rec liste_sommets graphe = 
 	match graphe with
 	| ((num,_)::suite) -> num::(liste_sommets suite)
@@ -19,6 +21,7 @@ let rec liste_sommets graphe =
 	(*cas du dernier sommet*)
 
 (* retourne le premier sommet du graphe*)
+(* val racine : ('a * 'b) list -> 'a * 'b = <fun> *)
 let rec racine graphe =
 	match graphe with
 	| ((num,succs)::_) -> (num,succs)
@@ -26,12 +29,14 @@ let rec racine graphe =
 ;;
 
 (* retourne le numero du sommet *)
+(* val numSommet : 'a * 'b -> 'a = <fun> *)
 let numSommet sommet = 
 	match sommet with
 	| (num,succs) -> num
 ;;
 
 (*retourne la liste des successeurs de sommet dans graphe*)
+(* val successeurs : ('a * 'b) list -> 'a -> 'b = <fun> *)
 let rec successeurs graphe sommet = 
 	match graphe with
 	| ((x, s)::r) -> if x = sommet then s else (successeurs r sommet)
@@ -41,6 +46,7 @@ let rec successeurs graphe sommet =
 
 
 (*retire les elements de l2 à l1*)
+(* val retirer : 'a list -> 'a list -> 'a list = <fun> *)
 let rec retirer l1 l2 =
 	match l1 with
 	| (x::r) -> if List.mem x l2 	
@@ -53,15 +59,15 @@ let rec retirer l1 l2 =
 (* parcours en profondeur *)
 let parcours_profondeur graphe =
 	let rec parcours_interne sommet dejaVisite pasEncoreVisite =
-		if (pasEncoreVisite=[])
-			then (*[]*)dejaVisite (* cas où l'on a fini de parcourir le graphe*)
+		if (pasEncoreVisite = [])
+			then dejaVisite
 			else	match sommet with
 			| (num,succs) -> if(List.mem num pasEncoreVisite) (* si le sommet n'est pas encore visité*)
 													then (*num::*)parcours_interne
 																(List.hd(retirer (succs@pasEncoreVisite) dejaVisite),successeurs graphe (List.hd(retirer (succs@pasEncoreVisite) dejaVisite))) (*on visite le prochain element de pas encore visité *)
-																(dejaVisite::num) (*on ajoute le sommet actuel dans la liste de ceux déjà visités*)
-																(retirer (succs@pasEncoreVisite) (dejaVisite::num))	(* alors on ajoute ses successeurs qui ne sont pas dans " déjà visité " dans " pas encore visité "*)
+																(num::dejaVisite) (*on ajoute le sommet actuel dans la liste de ceux déjà visités*)
+																(retirer (succs@pasEncoreVisite) (num::dejaVisite))	(* alors on ajoute ses successeurs qui ne sont pas dans " déjà visité " dans " pas encore visité "*)
 																(*on retire le sommet actuel de ceux pasEncoreVisités*)
-			|_ -> failwith "sommet invalide"
+													else []
 	in parcours_interne (racine graphe) [] [numSommet (racine graphe)]
 ;;
